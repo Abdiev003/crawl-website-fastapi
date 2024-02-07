9677614ad7eeda645e810859fdae90177e12ea1c
Ali Abdiyev <abdiev@Alis-Air.netis.cc>
2024-02-07 12:40:14 +0400

    feat: add page-content character count

--- app/api/api.py
+++ app/api/api.py
@@ -1,5 +1,5 @@
 from fastapi import HTTPException
-
+from copy import deepcopy
 from bs4 import BeautifulSoup as Soup
 from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
 
@@ -12,6 +12,16 @@ def load_url(url_input: UrlInput):
         loader = RecursiveUrlLoader(
             url=string_url, max_depth=100, extractor=lambda x: Soup(x, "html.parser").text)
         docs = loader.load()
-        return docs
+
+        updated_docs = []
+        for doc in docs:
+            doc_dict = doc.__dict__
+
+            character_count = len(doc_dict.get('page_content', ''))
+            doc_dict['character_count'] = character_count
+
+            updated_docs.append(doc_dict)
+
+        return updated_docs
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
