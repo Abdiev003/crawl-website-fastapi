b3a8e57d030fb738d72e090bd4ea56202cbb0e35
Ali Abdiyev <abdiev@Alis-Air.netis.cc>
2024-03-28 15:08:06 +0400

    change package.json file

--- app/api/api.py
+++ app/api/api.py
@@ -8,6 +8,7 @@ link_count = 0
 stop_loading = False
 stop_max_links = 10
 stop_max_characters = 400000
+total_character_count = 0
 
 
 def custom_extractor(html_content):
@@ -15,6 +16,9 @@ def custom_extractor(html_content):
     global stop_loading
     global stop_max_links
     global stop_max_characters
+    global total_character_count
+
+    print('Extracting content...')
 
     # XML dosyası kontrolü
     if html_content.strip().startswith('<?xml'):
@@ -22,13 +26,16 @@ def custom_extractor(html_content):
 
     soup = Soup(html_content, "html.parser")
 
-    if link_count == stop_max_links or len(soup.text) > stop_max_characters:
+    if link_count == stop_max_links or total_character_count > stop_max_characters:
         stop_loading = True
 
     if stop_loading:
         return None
 
     link_count = link_count + 1
+    total_character_count = total_character_count + len(soup.text)
+    print(
+        f"Link count: {link_count}, Character count: {total_character_count}")
 
     return soup.text
 
@@ -38,22 +45,24 @@ def load_url(url_input: UrlInput):
     global stop_max_characters
 
     if url_input.is_free:
-        stop_max_links = 2
+        stop_max_links = 10
         stop_max_characters = 400000
     else:
         stop_max_links = float('inf')
         stop_max_characters = 11000000
 
     exclude_dirs = (f'{url_input.url}wp-content/', f'{url_input.url}wp-includes/',
-                    f'{url_input.url}wp-json/', f'{url_input.url}comments/', f'{url_input.url}category/', f'{url_input.url}feed/')
+                    f'{url_input.url}wp-json/', f'{url_input.url}comments/', f'{url_input.url}category/', f'{url_input.url}feed/', f'{url_input.url}fonts/',)
 
     try:
         string_url = str(url_input.url)
         loader = RecursiveUrlLoader(
-            url=string_url, max_depth=8, prevent_outside=True, use_async=True, extractor=custom_extractor,
-            timeout=60, check_response_status=True, exclude_dirs=exclude_dirs
+            url=string_url, max_depth=8, prevent_outside=True, use_async=False, extractor=custom_extractor,
+            timeout=600, check_response_status=True, exclude_dirs=exclude_dirs,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
         )
-        docs = loader.lazy_load()
+        docs = loader.load()
 
         updated_docs = []
         total_character_count = 0
